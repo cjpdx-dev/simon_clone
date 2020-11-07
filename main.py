4906705525ea1f398dc0790f2ff3beab201e81db
cjpdx <cjpdx.dev@gmail.com>
2020-11-07 02:48:30 -0500

    updated .gitignore to include __pycache__, transitioned to a OOP format instead functionally from main.py, and implemented some (currently commented out) code that starts the game logo and blits some button images onto the screen. Button images were added to /images

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
 import pygame
+import Game
 
 
 def main():
 
-    game_surface_size = (440, 380)
+    game_surface_size = (720, 640)
+    button_image_paths = ["images/blue_square.png",
+                          "images/green_square.png",
+                          "images/orange_square.png",
+                          "images/red_square.png"]
 
-    # init the pygame module
-    pygame.init()
+    new_game = Game.Game()
+    new_game.on_execute()
 
     # setup the game logo
-    game_logo = pygame.image.load("images/logo.png")
-    pygame.display.set_icon(game_logo)
-    pygame.display.set_caption("Simon")
+    # game_logo = pygame.image.load("images/logo.png")
+    # pygame.display.set_icon(game_logo)
+    # pygame.display.set_caption("Simon")
 
     # create a game surface using game_surface_size arg
-    screen = pygame.display.set_mode(game_surface_size)
-
-    running = True
-    while running:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
+    # screen = pygame.display.set_mode(game_surface_size)
+    # blit_button_images = [pygame.image.load(image_path) for image_path in button_image_paths]
 
 
 # runs the main function only if the module is executed
